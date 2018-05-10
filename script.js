bcbc2bc22c886fb1e398ba3e04fa5da3b19d58b5
amanda <am.morang@gmail.com>
2018-05-10 11:59:24 -0400

    classes toggeling on click

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
+"use strict";
+
+$(document).ready(function(){ 
+
 //cards class="disease (differentiated)", class for face down, overall id for card, 
 
 
 //WHEN WINDOW LOADS, GAME STARTS
-window.onload = startGame();
-
-
+// window.onload = startGame();
 
 //ALL CARDS ARE STORED IN AN ARRAY
-let card = document.getElementsByClassName("card");
-let cards = [...card];
+// let card = document.getElementsByClassName("card");
+// let cards = [...card];
 
 // ON CLICKING A CARD IT WILL LOOP THROUGH ARRAY OF CARDS
+// FUNCTIONS THAT FLIP CARDS
+// let displayCard = function (){
+//   this.classList.toggle("face-down");
+//   this.classList.toggle("show");
+//   this.classList.toggle("disable");
+// }
 
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener("click", displayCard);
-}
+ $(document.body).on("click", "li.card", function(event) {
+   console.log(event.target);
+   $(event.target)
+    .toggleClass("face-down")
+    .toggleClass("show")
+    // .css("background-image", "url(https://slack-imgs.com/?c=1&url=http%3A%2F%2Fgjsmsvirtualzoo.pbworks.com%2Ff%2F1417702731%2Fkoala%2520%2520bear.jpg)");
+ })
 
+
+//  for (let i = 0; i < cards.length; i++) {
+//   cards[i].addEventListener("click", displayCard);
+// }
+
+
+
+
+/*
 
 //START GAME FUNCTION, LOOPS THROUGH ARRAY OF CARDS AND ADDS THEM TO WINDOW
 let deck = document.querySelector(".deck");
 function startGame() {
   let shuffleCards = shuffle(cards);
-
   for(let i = 0; i < shuffledCards.length; i++){
     [].forEach.call(shuffledCards, function(item) {
       deck.appendChild(item);
@@ -29,26 +49,13 @@ function startGame() {
   }
 }
 
-
-// DISPLAY 2 INSTANCES OF EACH CARD ON DISPLAY - DISPLAYCARD FUNCTION
+ 
 
 
 // ON RESET, THE STARTGAME() SHOULD BE CALLED AGAIN
 
 
 // FUNCTION TO HAVE INTRODUCTION OF GAME POP UP, THIS SHOULD ALSO WORK FOR RESET
-
-
-
-// FUNCTIONS THAT FLIP CARDS - WE SHOULD DO THIS IN HTML/CSS
-let displayCard = function (){
-  this.classList.toggle("face-down");
-  this.classList.toggle("show");
-  this.classList.toggle("disable");
-}
-
-
-
 
 
 
@@ -102,12 +109,9 @@ function cardOpen() {
   }
 
 
-// CONGRATS POP UP FOR WHEN ALL CARDS MATCH
-
-
-
 }
+*/
 
 
 
-
+});

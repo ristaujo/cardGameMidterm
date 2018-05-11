92ae827d60c3212d48be2150577b3de06c3bba0c
elle <ellegover@gmail.com>
2018-05-11 13:13:16 -0400

    worked on functionality

--- script.js
+++ script.js
@@ -6,31 +6,50 @@ $(document).ready(function(){
 
 
 const startGame = () => {
+  $('#timer').show();
+  $('#timer').html(`20`);
+  // $(".blank").toggleClass("blank face-down")
+  // $(".show").toggleClass("show face-down")
 
+  let matches = 0;
+  
   console.log("game started");
-  $(".blank").toggleClass("blank face-down");
-  $(".show").toggleClass("show face-down");
 
   function countdown() {
 
-    let seconds = 90;
+    let seconds = $("#timer").text();
     let ourTimer = setInterval( () => {
       startTime()
     }, 1000);
 
     const startTime = () => {
-      if (seconds == 0) {
+      // console.log(matches);
+      
+      if (seconds == 0 && matches != 2) {
         clearInterval(ourTimer);
         console.log("timer done");
-        // $("#timer").text("Oh no!");
-        // $("#popup").html(`<p>You could not save the world.</p>`)
-        // .show();
+        $("#timer").text("Oh no!");
+        $("#popup").html(`<p>You could not save the world.</p>`)
+        .show();
+      } else if (matches === 2) {
+        console.log("you win!")
+        $("#popup").html(`<p>You saved the world.</p>`)
+        .show();
+        clearInterval(ourTimer);
       } else {
         seconds--;
-        console.log(seconds);
+        // console.log(seconds);
+        // console.log($("#timer").text());
         $("#timer").html(seconds);
       }
     }
+       // reset button
+   $(document.body).on("click", "#reset-button", function(event) {
+    clearInterval(ourTimer);
+    // $("#popup").hide();
+    // startGame();
+    brandNewGame();
+  });
   } 
   
   countdown();
@@ -42,15 +61,14 @@ const startGame = () => {
   for (let i = 1; i < count; i++) {
     let j = Math.floor(Math.random() * count);
     $('li', list).eq(j).appendTo(list);
-} console.log(`this is the list ${list}`);
+} 
   }
   shuffle();
-  let matches = 0;
   let timer = 2;
 
     let showCards = [];
     let clickedCard;
-    let matchCards = 0;
+    
   // begin matching cards
   $(document.body).on("click", "li.card", function(event) {
 
@@ -60,25 +78,21 @@ const startGame = () => {
     showCards.push(clickedCard);
     // compare once we click 2 cards
     if (showCards.length === 2) {
+      console.log(showCards);
+      
       if (showCards[0] === showCards[1]) {
         console.log("match");
         $(".show").toggleClass("show blank");
         showCards = [];
-        matchCards++;
-        if (matchCards === 8) {
-          console.log("you won!");
-          $("#timer").hide();
-          $("#timer").text("Yay!");
-          $("#popup").html(`<p>You saved the world.</p>`)
-          .show();
-          // clearInterval(ourTimer);
-        } else if ($("#timer").text === 0) {
-          $("#timer").text("Oh no!");
-          $("#popup").html(`<p>You could not save the world.</p>`)
-          .show();
-
-        }
-        console.log(matchCards);
+        matches++;
+        // if (matches === 2) {
+        //   console.log("you won!");
+        //   $("#timer").hide();
+        //   $("#timer").text("Yay!");
+        //   $("#popup").html(`<p>You saved the world.</p>`)
+        //   .show();
+        // } 
+        console.log(matches);
       } else {
         $(".show").toggleClass("show face-down");
         showCards = [];
@@ -94,17 +108,22 @@ const startGame = () => {
 
 ////////////////////START OF BRAND NEW GAME
 const brandNewGame = () => {
-  $('#timer').html(`90`);
+  $(".blank").toggleClass("blank face-down")
+  $(".show").toggleClass("show face-down")
+  let matches = 0;
+  // $('#timer').show();
+  // $('#timer').html(`20`);
   $("#popup").html(`<p>You ARE patient zero.</p><button type="button" id="start-button">Plague</button>`)
+  .show();
 $(document.body).on("click", "#start-button", function(event) 
 {
   $("#popup").hide();
   startGame()
 }); 
-   // reset button
-   $(document.body).on("click", "#reset-button", function(event) {
-    brandNewGame();
-  });
+  //  // reset button
+  //  $(document.body).on("click", "#reset-button", function(event) {
+  //   brandNewGame();
+  // });
 
 }
 

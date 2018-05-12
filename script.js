d7b27c6b2e3a3c8c09fee2ec2354b18d7f7e09c2
jacki <jackiaolson@gmail.com>
2018-05-12 12:09:05 -0400

    fixed card delay bug, added reset functionality, added max-width on mobile

--- script.js
+++ script.js
@@ -2,140 +2,130 @@
 
 $(document).ready(function(){ 
 
+  const startGame = () => {
+    // shows the timer and sets the length of game time in seconds
+    let seconds = 300;
+    // $('#timer').html(`${seconds}`);
 
+    let matches = 0;
+    
+    console.log("game started");
 
+    function countdown() {
 
-const startGame = () => {
-  $('#timer').show();
-  $('#timer').html(`20`);
-  // $(".blank").toggleClass("blank face-down")
-  // $(".show").toggleClass("show face-down")
-
-  let matches = 0;
-  
-  console.log("game started");
-
-  function countdown() {
-
-    let seconds = $("#timer").text();
-    let ourTimer = setInterval( () => {
-      startTime()
-    }, 1000);
-
-    const startTime = () => {
-      // console.log(matches);
-      
-      if (seconds == 0 && matches != 2) {
-        clearInterval(ourTimer);
-        console.log("timer done");
-        $("#timer").text("Oh no!");
-        $("#popup").html(`<p>You could not save the world.</p>`)
-        .show();
-      } else if (matches === 2) {
-        console.log("you win!")
-        $("#popup").html(`<p>You saved the world.</p>`)
-        .show();
-        clearInterval(ourTimer);
-      } else {
-        seconds--;
-        // console.log(seconds);
-        // console.log($("#timer").text());
-        $("#timer").html(seconds);
+      // this displays the initial time in minutes and seconds
+      let justSeconds = seconds%60;
+      // console.log(time);
+      if ((justSeconds) < 10 && seconds >= 0) {
+        justSeconds = "0" + justSeconds;
       }
-    }
-       // reset button
-   $(document.body).on("click", "#reset-button", function(event) {
-    clearInterval(ourTimer);
-    // $("#popup").hide();
-    // startGame();
-    brandNewGame();
-  });
-  } 
-  
-  countdown();
+      $("#timer").text(`${Math.floor(seconds/60)}:${justSeconds}`);
+      // let seconds = $("#timer").text();
+      // END INITIAL TIME DISPLAY
 
-  const shuffle = () => {
-    console.log("shuffle");
-    let list = $('ul');
-    let count = $('ul li').length;
-  for (let i = 1; i < count; i++) {
-    let j = Math.floor(Math.random() * count);
-    $('li', list).eq(j).appendTo(list);
-} 
-  }
-  shuffle();
-  let timer = 2;
+      let ourTimer = setInterval( () => {
+        startTime()
+      }, 1000);
 
+      const startTime = () => {
+        // console.log(matches);
+        
+        if (seconds == 0 && matches != 2) {
+          clearInterval(ourTimer);
+          console.log("timer done");
+          $("#timer").text("Oh no!");
+          $("#popup").html(`<p>You could not save the world.</p>`)
+          .show();
+        } else if (matches === 2) {
+          console.log("you win!")
+          $("#popup").html(`<p>You saved the world.</p>`)
+          .show();
+          clearInterval(ourTimer);
+        } else {
+          seconds--;
+          // console.log($("#timer").text());
+
+          // this part displays the time in minutes and seconds
+          let time = seconds%60;
+          // console.log(time);
+          if ((time) < 10 && seconds >= 0) {
+            time = "0" + time;
+          }
+          $("#timer").text(`${Math.floor(seconds/60)}:${time}`);
+        }
+      } 
+      // Reset Button
+    $(document.body).on("click", "#reset-button", function(event) {
+      // clearInterval(ourTimer);
+      // $("#popup").hide();
+      // startGame();
+      // brandNewGame();
+        location.reload(true);
+      });
+    } // end of countdown function
+    
+    countdown();
+
+    const shuffle = () => {
+      console.log("shuffle");
+      let list = $('ul');
+      let count = $('ul li').length;
+      for (let i = 1; i < count; i++) {
+      let j = Math.floor(Math.random() * count);
+      $('li', list).eq(j).appendTo(list);
+      } 
+    } // end of shuffle function
+    shuffle();
     let showCards = [];
     let clickedCard;
-    
-  // begin matching cards
-  $(document.body).on("click", "li.card", function(event) {
-
-    // flips the card over
-    $(event.target).toggleClass("face-down show open").delay(500);
-    clickedCard = $(event.target).attr("class");
-    showCards.push(clickedCard);
-    // compare once we click 2 cards
-    if (showCards.length === 2) {
-      console.log(showCards);
       
+    // begin matching cards
+    $(document.body).on("click", "li.card", function(event) {
 
-      const compare = () => {
-        if (showCards[0] === showCards[1]) {
-        console.log("match");
-        $(".show").toggleClass("show blank open");
-        showCards = [];
-        matches++;
-        // if (matches === 2) {
-        //   console.log("you won!");
-        //   $("#timer").hide();
-        //   $("#timer").text("Yay!");
-        //   $("#popup").html(`<p>You saved the world.</p>`)
-        //   .show();
-        // } 
-        console.log(matches);
-      } else {
-        $(".show").toggleClass("show face-down");
-        showCards = [];
-    }
-  } // end of function "compare"
-    setTimeout(compare, 500);
-    } // end of compare 2 cards
+      // flips the card over on click
+      $(event.target).toggleClass("face-down show open");
+      clickedCard = $(event.target).attr("class");
+      showCards.push(clickedCard);
+      // compare once we click 2 cards
+      if (showCards.length === 2) {
+        // console.log(showCards);
+        
 
+        const compare = () => {
+          if (showCards[0] === showCards[1]) {
+          console.log("match");
+          $(".show").toggleClass("show blank open");
+          showCards = [];
+          matches++;
+          console.log(matches);
+        } else {
+          $(".show").toggleClass("show face-down open");
+          showCards = [];
+        }
+    } // end of compare function
 
-  });
+      // after 3 seconds, run compare function
+      setTimeout(compare, 3000);
+      } // end of compare 2 cards
+    }); // end of click on card functionality
 
-} // end of start game
-
- 
-
-////////////////////START OF BRAND NEW GAME
-const brandNewGame = () => {
-  $(".blank").toggleClass("blank face-down")
-  $(".show").toggleClass("show face-down")
-  let matches = 0;
-  // $('#timer').show();
-  // $('#timer').html(`20`);
-  $("#popup").html(`<p>You ARE patient zero.</p><button type="button" id="start-button">Plague</button>`)
-  .show();
-$(document.body).on("click", "#start-button", function(event) 
-{
-  $("#popup").hide();
-  startGame()
-}); 
-  //  // reset button
-  //  $(document.body).on("click", "#reset-button", function(event) {
-  //   brandNewGame();
-  // });
-
-}
+  } // end of startGame function
 
 
+  const brandNewGame = () => {
+    $(".blank").toggleClass("blank face-down")
+    $(".show").toggleClass("show face-down")
+    let matches = 0;
+    $("#popup").html(`<p>You ARE patient zero.</p><button type="button" id="start-button">Plague</button>`)
+    .show();
+    $(document.body).on("click", "#start-button", function(event) {
+      $("#popup").hide();
+      startGame();
+    }); 
+  } // end of brandNewGame function
 
-brandNewGame();
+  //////////////////// Start of actual code that runs on load
 
-  // startGame();
+  brandNewGame();
 
-
-});
+}); // end of program
